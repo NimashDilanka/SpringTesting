98f85c2db364ec5e5d5fc0a98d1597fdac2bb021
Nimash Dilanka <nimash@codegen.net>
2018-07-06 13:57:46 +0530

    NIM5:setup inject literal values

--- src/main/java/com/lov2code/springdemo/CricketCoach.java
+++ src/main/java/com/lov2code/springdemo/CricketCoach.java
@@ -3,13 +3,39 @@ package com.lov2code.springdemo;
 public class CricketCoach implements Coach
 {
     public FortuneService fortuneService;
+    private String emailAddress;
+    private String team;
+
     @Override public String getDailyWorkOut()
     {
-        return "\n\n\ncricket coach get daily workout\n\n\n";
+        return "\n\n\ncricket coach get daily workout"+
+                "team: "+team+
+                "email address: "+emailAddress+"" +
+                "\n\n\n";
     }
 
     public void setFortuneService( FortuneService fortuneService )
     {
         this.fortuneService = fortuneService;
+    }
+
+    public void setEmailAddress( String emailAddress )
+    {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam( String team )
+    {
+        this.team = team;
+    }
+
+    public String getEmailAddress()
+    {
+        return emailAddress;
+    }
+
+    public String getTeam()
+    {
+        return team;
     }
 }
