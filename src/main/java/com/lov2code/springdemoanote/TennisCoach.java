609cd3292b27ae44101c6480c27de897ade58dd9
Nimash Dilanka <nimash@codegen.net>
2018-07-11 14:00:50 +0530

    NIM12:AutoWiring-setter injection

--- src/main/java/com/lov2code/springdemoanote/TennisCoach.java
+++ src/main/java/com/lov2code/springdemoanote/TennisCoach.java
@@ -9,20 +9,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach
 {
-    private final FortuneService fortuneService;
+    private FortuneService fortuneService;
 
-    //As of Spring Framework 4.3, an @Autowired annotation on such a constructor is no longer necessary
-    //if the target bean only defines one constructor to begin with. However, if several constructors are available,
-    // at least one must be annotated to teach the container which one to use.I personally prefer to use the @Autowired
-    // annotation because it makes the code more readable. But as mentioned, the @Autowired is not required for this scenario.
-    //@Autowired  //constructor injection-auto wired by Happy fortune service
-    public TennisCoach( FortuneService fortuneService )
-    {
-        this.fortuneService = fortuneService;
-    }
     @Override public String getDailyWorkOut()
     {
         return "\n\nget the tennis coach\n\n\n"+fortuneService.getFortuneService();
+    }
+
+    @Autowired
+    public void setFortuneService( FortuneService fortuneService )
+    {
+        this.fortuneService = fortuneService;
     }
 
     //adding a init method
