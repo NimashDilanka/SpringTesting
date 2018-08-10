a55956467d82be8c8af0923824f0401d78ba4f44
Nimash Dilanka <nimash@codegen.net>
2018-08-10 14:10:10 +0530

    NIM48:hibernate setup

---
+++ src/main/java/com/lov2code/springmvc/hibernate/entityset/Student.java
@@ -0,0 +1,24 @@
+package com.lov2code.springmvc.hibernate.entityset;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student")
+public class Student
+{
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lasttName;
+
+    @Column(name = "email")
+    private String email;
+}
